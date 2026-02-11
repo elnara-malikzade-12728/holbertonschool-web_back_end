8f041f5e53f987e2bde097d50296366b5e613241
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-11 07:59:35 +0000

    Delete whitespace in  3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,7 +6,7 @@ This module provides type-annotated function.
 
 def to_str(n: float) -> str:
     """
-    This function takes a float as an argument 
+    This function takes a float as an argument
     and returns the string representation of the float.
     """
     return str(n)
