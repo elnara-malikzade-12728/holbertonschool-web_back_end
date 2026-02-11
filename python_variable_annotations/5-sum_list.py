15474db7b35ac78fd2b25ffe5fb831e12cde3a74
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-11 08:54:57 +0000

    Fix 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 This module provides type-annotated function fo addition.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     This function takes a list of floats as argument 
     and returns their sum as float.
