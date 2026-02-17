bdcb9f345b96437164ae1541041b9b5a68e9498e
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-17 06:36:00 +0000

    Add blank line in 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-This module provides a function to execute multiple coroutines at the same time.
+This module provides a function to execute
+multiple coroutines at the same time.
 """
 import asyncio
 from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay.
-    Returns the list of all the delays (float values) in ascending order.
+    Returns the list of all the delays (float values)
+    in ascending order.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
 
