3b4361a28e61925885736f74e520a919f832fcff
Elnara Malikzade <12728@holbertonstudents.com>
2026-02-17 06:38:36 +0000

    Add blank line in 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,8 +6,8 @@ multiple coroutines at the same time.
 import asyncio
 from typing import List
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
