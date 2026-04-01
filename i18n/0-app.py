77fc9406b91eec43b97d4d2c576b6596f07eb5b3
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-01 09:40:15 +0000

    Modify shebang in file

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,4 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/env python3
+"""
+Basic flask app implementation
+"""
 from flask import Flask, render_template
 
 
@@ -6,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__  == '__main__':
-    app.run(debug=True, host=0.0.0.0, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
