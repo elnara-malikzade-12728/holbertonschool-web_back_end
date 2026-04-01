323141599648343a02cf9947b50c322021537234
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-01 09:57:15 +0000

    Remove additional space

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """
@@ -14,5 +15,6 @@ def home():
     """
     return render_template('0-index.html')
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
