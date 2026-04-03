f02033e4a62377d154798522b7419bd433ae80d9
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-03 12:26:39 +0000

    Add methods to 5-app.py

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""                                                                                                                                  
-This module is for Babel object instantiation                                                                                        
 """
-from flask import Flask, request, render_template
+This module is for Babel object instantiation
+"""
+from flask import Flask, request, render_template, g
 from flask_babel import Babel
 
 
@@ -15,8 +15,8 @@ users = {
 
 
 class Config:
-    """                                                                                                                              
-    This class is for configuring the languages                                                                                      
+    """
+    This class is for configuring the languages
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
@@ -24,8 +24,8 @@ class Config:
 
 
 def get_locale():
-    """                                                                                                                              
-    Determines the best match for supported languages                                                                                
+    """
+    Determines the best match for supported languages
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
@@ -39,10 +39,32 @@ app.config.from_object(Config)
 babel = Babel(app, locale_selector=get_locale)
 
 
+def get_user(self, id):
+    """
+    A method to return a user dictionary or None if there is not any user
+    """
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    else:
+        return None
+
+
+@app.before_request
+def before_request():
+    """
+    A method to be used before every method,
+    that finds a user and sets it as a global user
+    """
+    user_id = request.args.get('login_as')
+    user = get_user(user_id)
+    g.user = user
+
+
 @app.route('/')
 def home():
-    """                                                                                                                              
-    Renders the template                                                                                                             
+    """
+    Renders the template
     """
     login_id = request.args.get('login_as')
     user = users.get(int(login_id)) if login_id else None
