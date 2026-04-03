a3d713d854fd33e2ba8f4b916ac65878f76ec8f8
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-03 17:55:18 +0000

    Add 5-app.py

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -2,9 +2,9 @@
 """
 This module is for Babel object instantiation
 """
-from flask import Flask, request, render_template, g
-from flask_babel import Babel
 
+from flask import Flask, request, render_template, g
+from flask_babel import Babel, _
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -19,45 +19,55 @@ class Config:
     This class is for configuring the languages
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=lambda: get_locale())
 
 
 def get_locale():
     """
     Determines the best match for supported languages
     """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
         return locale
     else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 def get_user():
-    """ Returns a user dictionary or None if ID is invalid """
-    login_id = request.args.get('login_as')
-    if login_id:
+    """
+    Returns a user dictionary or None if ID is invalid
+    """
+    login_id = request.args.get("login_as")
+    if login_id is None:
+        return None
+
+    try:
         return users.get(int(login_id))
-    return None
+    except (ValueError, TypeError):
+        return None
 
 
 @app.before_request
 def before_request():
-    """ Sets the found user as a global on flask.g.user """
+    """
+    Sets the found user as a global on flask.g.user
+    """
     g.user = get_user()
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    """ Renders the template """
-    return render_template('5-index.html')
+    """
+    Renders the template
+    """
+    return render_template("5-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
