78fcb9456bc37ae4db5adfa162b23ed62eabe31c
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-03 08:47:34 +0000

    Fix the 1-app.py file

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -17,7 +17,7 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app, locale_selector=BABEL_DEFAULT_LOCALE)
 
 
 @app.route('/')
