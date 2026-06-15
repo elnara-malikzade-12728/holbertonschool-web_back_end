171d446a8571041fac865b050d7b1909d2d99fdc
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-15 09:30:08 +0400

    Add fixed files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  // Keep \r here because the test expects it
+  process.stdout.write(`Your name is: ${name}\r`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  // Add \n here at the beginning to prevent overwriting the line above
+  process.stdout.write('\nThis important software is now closing\n');
 });
