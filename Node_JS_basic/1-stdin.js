0774fe1a4b30cf6e5e0344ab5828c3c487fe5883
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-15 09:15:22 +0400

    Add fixed files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString();
+  const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write('\n');
 });
 
 process.stdin.on('end', () => {
