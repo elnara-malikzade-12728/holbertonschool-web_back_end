2d13614205d50961b2178c057c5f5efafd5ff0a9
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-18 17:33:41 +0400

    Add task8

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const PORT = 7865;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the payment system' });
+  res.send('Welcome to the payment system');
 });
 
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,14 +1,15 @@
-const api = require('./api');
 const request = require('supertest');
-const {expect} = require('chai')
+const { expect } = require('chai');
+const api = require('./api');
 
 describe('API integration tests', () => {
-  it('should return welcome message on GET /', async () => {
+  it('Correct status code?', async () => {
     const response = await request(api).get('/');
     expect(response.status).to.equal(200);
+  });
+
+  it('Correct result?', async () => {
+    const response = await request(api).get('/');
     expect(response.text).to.equal('Welcome to the payment system');
   });
 });
-
-module.exports = app;
-
