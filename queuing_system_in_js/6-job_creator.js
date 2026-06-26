8ba39b0c010d635af63a0736ca0e497780d08d00
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-26 13:13:16 +0400

    Fix the file

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -1,19 +1,12 @@
 import kue from 'kue';
+
 const queue = kue.createQueue();
 
-const jobData = {
-  phoneNumber: '1234567890',
-  message: 'Your notification message is here.'
-};
-const job = queue.create('push_notification_code', jobData);
-job.on('complte', (result) => {
+const job = queue.create('push_notification_code', {
+  phoneNumber: '4153518780',
+  message: 'This is the code 1234 to verify your account',
+}).save((err) => {
+  if (!err) {
     console.log(`Notification job created: ${job.id}`);
-});
-job.on('failed', (errorMessage) => {
-    console.log(`Notification job failed`);
-  if (!err){
-    setImmediate(() =>{
-        console.log(`Notification job created: ${job.id}`);
-    });
   }
 });
