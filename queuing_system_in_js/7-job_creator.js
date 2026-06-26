49e415d079a0c3468bf09f2a66e598c6cd54efa9
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-26 14:07:44 +0400

    Add task9

--- queuing_system_in_js/7-job_creator.js
+++ queuing_system_in_js/7-job_creator.js
@@ -48,17 +48,17 @@ const jobs = [
   }
 ];
 jobs.forEach((jobData) => {
-  const job = queue.create('push_notification_code', jobData).save((err) => {
+  const job = queue.create('push_notification_code_2', jobData).save((err) => {
     if (!err) console.log(`Notification job created: ${job.id}`);
   });
   job.on('complete', () => {
-    console.log(`Notification job ${job.id} completed`);
+    console.log(`Notification job #${job.id} completed`);
   });
   job.on('failed', (err) => {
-    console.log(`Notification job ${job.id} failed: ${err}`);
+    console.log(`Notification job #${job.id} failed: ${err}`);
   }); 
   job.on('progress', (progress) => {
-    console.log(`Notification job ${job.id} ${progress}% complete`);
+    console.log(`Notification job #${job.id} ${progress}% complete`);
   });
 });
 
