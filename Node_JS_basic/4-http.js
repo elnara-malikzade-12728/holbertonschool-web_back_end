0707a2c5bfe8860d2fe0795cd12ba2eddf392909
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-27 18:10:56 +0400

    Add task4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
+  if (req.url === '/'|| req.url === '/test') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   }
