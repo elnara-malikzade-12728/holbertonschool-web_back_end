f730f257f3ee948336876e8bd137521b69003e1f
Elnara Malikzade <12728@holbertonstudents.com>
2026-06-27 18:08:50 +0400

    Add task4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
